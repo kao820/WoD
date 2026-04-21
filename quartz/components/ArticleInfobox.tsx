aa9210eda2342137933d77af5b9dc6e63448013c
Algetmargh <63553318+kao820@users.noreply.github.com>
2026-04-21 18:28:31 +0300

    Fix chronicle mask icons rendering in WebKit/Blink

--- quartz/components/ArticleInfobox.tsx
+++ quartz/components/ArticleInfobox.tsx
@@ -314,33 +314,42 @@ ArticleInfobox.css = `
   margin-left: auto;
   background: currentColor;
   opacity: 0.92;
+  -webkit-mask-position: center;
+  -webkit-mask-repeat: no-repeat;
+  -webkit-mask-size: contain;
   mask-position: center;
   mask-repeat: no-repeat;
   mask-size: contain;
 }
 
 .wiki-infobox__chronicle-icon--mage {
-  mask-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24'%3E%3Cpath d='M3 5h8c1.3 0 2.5.5 3.5 1.3C15.5 5.5 16.7 5 18 5h3v14h-3c-1.5 0-2.8.5-3.8 1.4L14 20l-.2.4C12.8 19.5 11.5 19 10 19H3V5zm2 2v10h5c1 0 1.9.2 2.8.6V7.8c-.8-.5-1.7-.8-2.8-.8H5zm14 0h-1c-1.1 0-2 .3-2.8.8v9.8c.9-.4 1.8-.6 2.8-.6h1V7z'/%3E%3C/svg%3E");
+  -webkit-mask-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24'%3E%3Cpath d='M12 2l1.7 4.3L18 8l-4.3 1.7L12 14l-1.7-4.3L6 8l4.3-1.7L12 2zm-6 11l1 2.5L9.5 16 7 17l-1 2.5L5 17l-2.5-1L5 15.5 6 13zm12 0l1 2.5 2.5.5-2.5 1-1 2.5-1-2.5-2.5-1 2.5-.5 1-2.5zM12 11l1.2 3 3 .8-3 1.2-1.2 3-1.2-3-3-1.2 3-.8 1.2-3z'/%3E%3C/svg%3E");
+  mask-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24'%3E%3Cpath d='M12 2l1.7 4.3L18 8l-4.3 1.7L12 14l-1.7-4.3L6 8l4.3-1.7L12 2zm-6 11l1 2.5L9.5 16 7 17l-1 2.5L5 17l-2.5-1L5 15.5 6 13zm12 0l1 2.5 2.5.5-2.5 1-1 2.5-1-2.5-2.5-1 2.5-.5 1-2.5zM12 11l1.2 3 3 .8-3 1.2-1.2 3-1.2-3-3-1.2 3-.8 1.2-3z'/%3E%3C/svg%3E");
 }
 
 .wiki-infobox__chronicle-icon--changeling {
-  mask-image: url(\"data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24'%3E%3Cpath d='M12 6c1.1 0 2 .9 2 2 0 .8-.5 1.5-1.2 1.8V12l1.8 2.7h-1.8L12 13l-.8 1.7H9.4L11 12V9.8A2 2 0 0 1 12 6zM5 9c2.3 0 4.2.9 5.3 2.4-.5.3-.9.8-1.2 1.3C8.2 11.4 6.7 11 5 11H3V9h2zm14 0h2v2h-2c-1.7 0-3.2.4-4.1 1.7-.3-.5-.7-1-1.2-1.3C14.8 9.9 16.7 9 19 9zM5.5 14c1.6 0 2.8.4 3.6 1.5.5.7 1.2 1.2 2 1.4v2.1c-1.6-.2-3.1-1-4.1-2.4C6.4 15.7 5.5 15.4 4 15.4H2V14h3.5zm13 0H22v1.4h-2c-1.5 0-2.4.3-3 .9-1 1.4-2.5 2.2-4.1 2.4v-2.1c.8-.2 1.5-.7 2-1.4.8-1.1 2-1.5 3.6-1.5z'/%3E%3C/svg%3E\");
+  -webkit-mask-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24'%3E%3Cpath d='M12 3c2.8 0 5 2.2 5 5 0 1.9-1 3.5-2.6 4.4l-.4 3.1 2.5-1.4-.6 3.2-3 1.7L12 22l-1-3-3-1.7-.6-3.2 2.5 1.4-.4-3.1A5 5 0 0 1 7 8c0-2.8 2.2-5 5-5zm0 2.2A2.8 2.8 0 1 0 12 11a2.8 2.8 0 0 0 0-5.6z'/%3E%3C/svg%3E");
+  mask-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24'%3E%3Cpath d='M12 3c2.8 0 5 2.2 5 5 0 1.9-1 3.5-2.6 4.4l-.4 3.1 2.5-1.4-.6 3.2-3 1.7L12 22l-1-3-3-1.7-.6-3.2 2.5 1.4-.4-3.1A5 5 0 0 1 7 8c0-2.8 2.2-5 5-5zm0 2.2A2.8 2.8 0 1 0 12 11a2.8 2.8 0 0 0 0-5.6z'/%3E%3C/svg%3E");
 }
 
 .wiki-infobox__chronicle-icon--demon {
-  mask-image: url(\"data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24'%3E%3Cpath d='M12 2l2.6 5.4 6 .9-4.3 4.2 1 5.9-5.3-2.8-5.3 2.8 1-5.9L3.4 8.3l6-.9L12 2zm0 3.8L10.5 9h-3.7l2.8 2.7-.7 3.8 3.1-1.7 3.1 1.7-.7-3.8 2.8-2.7h-3.7L12 5.8z'/%3E%3C/svg%3E\");
+  -webkit-mask-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24'%3E%3Cpath d='M7 3c1.3 1.4 2 2.6 2 4.1C9 8.7 7.9 10 6.2 10.9A4 4 0 0 0 8 14.3V19l4-2 4 2v-4.7a4 4 0 0 0 1.8-3.4c-1.7-.9-2.8-2.2-2.8-3.8 0-1.5.7-2.7 2-4.1L13.6 5 12 3.5 10.4 5 7 3zm5 5.7c1.8 0 3.3 1.5 3.3 3.3S13.8 15.3 12 15.3 8.7 13.8 8.7 12 10.2 8.7 12 8.7z'/%3E%3C/svg%3E");
+  mask-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24'%3E%3Cpath d='M7 3c1.3 1.4 2 2.6 2 4.1C9 8.7 7.9 10 6.2 10.9A4 4 0 0 0 8 14.3V19l4-2 4 2v-4.7a4 4 0 0 0 1.8-3.4c-1.7-.9-2.8-2.2-2.8-3.8 0-1.5.7-2.7 2-4.1L13.6 5 12 3.5 10.4 5 7 3zm5 5.7c1.8 0 3.3 1.5 3.3 3.3S13.8 15.3 12 15.3 8.7 13.8 8.7 12 10.2 8.7 12 8.7z'/%3E%3C/svg%3E");
 }
 
 .wiki-infobox__chronicle-icon--werewolf {
-  mask-image: url(\"data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24'%3E%3Cpath d='M7 4a2 2 0 1 1 0 4 2 2 0 0 1 0-4zm5-2a2 2 0 1 1 0 4 2 2 0 0 1 0-4zm5 2a2 2 0 1 1 0 4 2 2 0 0 1 0-4zM5.5 8.5a2 2 0 1 1 0 4 2 2 0 0 1 0-4zM12 8c3.2 0 5.5 2 5.5 5 0 2.6-2 4.8-5.5 4.8S6.5 15.6 6.5 13c0-3 2.3-5 5.5-5z'/%3E%3C/svg%3E\");
+  -webkit-mask-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24'%3E%3Cpath d='M5.5 9.5a2 2 0 1 1 0-4 2 2 0 0 1 0 4zm4-2a2 2 0 1 1 0-4 2 2 0 0 1 0 4zm5 0a2 2 0 1 1 0-4 2 2 0 0 1 0 4zm4 2a2 2 0 1 1 0-4 2 2 0 0 1 0 4zM12 9c3.7 0 6 2.3 6 5.3C18 17.5 15.4 20 12 20s-6-2.5-6-5.7C6 11.3 8.3 9 12 9zm-2 3.5a1.3 1.3 0 1 0 0 2.6 1.3 1.3 0 0 0 0-2.6zm4 0a1.3 1.3 0 1 0 0 2.6 1.3 1.3 0 0 0 0-2.6z'/%3E%3C/svg%3E");
+  mask-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24'%3E%3Cpath d='M5.5 9.5a2 2 0 1 1 0-4 2 2 0 0 1 0 4zm4-2a2 2 0 1 1 0-4 2 2 0 0 1 0 4zm5 0a2 2 0 1 1 0-4 2 2 0 0 1 0 4zm4 2a2 2 0 1 1 0-4 2 2 0 0 1 0 4zM12 9c3.7 0 6 2.3 6 5.3C18 17.5 15.4 20 12 20s-6-2.5-6-5.7C6 11.3 8.3 9 12 9zm-2 3.5a1.3 1.3 0 1 0 0 2.6 1.3 1.3 0 0 0 0-2.6zm4 0a1.3 1.3 0 1 0 0 2.6 1.3 1.3 0 0 0 0-2.6z'/%3E%3C/svg%3E");
 }
 
 .wiki-infobox__chronicle-icon--hunter {
-  mask-image: url(\"data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24'%3E%3Cpath d='M11 3h2v4h3v2h-3v3h4v2h-4v7h-2v-7H7v-2h4V9H8V7h3V3z'/%3E%3C/svg%3E\");
+  -webkit-mask-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24'%3E%3Cpath d='M11 2h2v3.1a7 7 0 0 1 5.9 5.9H22v2h-3.1a7 7 0 0 1-5.9 5.9V22h-2v-3.1a7 7 0 0 1-5.9-5.9H2v-2h3.1a7 7 0 0 1 5.9-5.9V2zm1 5a5 5 0 1 0 0 10 5 5 0 0 0 0-10zm-1 2h2v2h2v2h-2v2h-2v-2H9v-2h2V9z'/%3E%3C/svg%3E");
+  mask-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24'%3E%3Cpath d='M11 2h2v3.1a7 7 0 0 1 5.9 5.9H22v2h-3.1a7 7 0 0 1-5.9 5.9V22h-2v-3.1a7 7 0 0 1-5.9-5.9H2v-2h3.1a7 7 0 0 1 5.9-5.9V2zm1 5a5 5 0 1 0 0 10 5 5 0 0 0 0-10zm-1 2h2v2h2v2h-2v2h-2v-2H9v-2h2V9z'/%3E%3C/svg%3E");
 }
 
 .wiki-infobox__chronicle-icon--vampire {
-  mask-image: url(\"data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24'%3E%3Cpath d='M4 8c0-2.2 1.8-4 4-4h8c2.2 0 4 1.8 4 4v2c0 3.9-3.1 7-7 7h-2c-3.9 0-7-3.1-7-7V8zm2 1v1c0 2.8 2.2 5 5 5h2c2.8 0 5-2.2 5-5V9H6zm3.2 0h2l-.6 3H9.7l-.5-3zm4.4 0h2l-.5 3h-.9l-.6-3z'/%3E%3C/svg%3E\");
+  -webkit-mask-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24'%3E%3Cpath d='M6 4h12l-1.2 6.8A4.8 4.8 0 0 1 12 15a4.8 4.8 0 0 1-4.8-4.2L6 4zm2.4 2 .7 4a2.9 2.9 0 0 0 5.8 0l.7-4H8.4zM9 16h2l-1 4H8l1-4zm4 0h2l1 4h-2l-1-4z'/%3E%3C/svg%3E");
+  mask-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24'%3E%3Cpath d='M6 4h12l-1.2 6.8A4.8 4.8 0 0 1 12 15a4.8 4.8 0 0 1-4.8-4.2L6 4zm2.4 2 .7 4a2.9 2.9 0 0 0 5.8 0l.7-4H8.4zM9 16h2l-1 4H8l1-4zm4 0h2l1 4h-2l-1-4z'/%3E%3C/svg%3E");
 }
 
 .wiki-infobox[class*="wiki-infobox--chronicle-"] {
